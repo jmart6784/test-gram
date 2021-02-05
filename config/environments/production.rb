064c4c36ddeec0d70cbac4e13c4b54cd327b540b
jmart6784 <jmartinez6784@yahoo.com>
2021-02-04 19:52:26 -0800

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Sendgrid
+  config.action_mailer.default_url_options = { :host => 'instagram-test-aws.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "instagram-test-aws.herokuapp.com",
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
